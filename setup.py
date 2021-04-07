bf276f49799ab5741a14bb3adc36579e3ad3535f
Oleksandr Sirenko <oleksa.sirenko@gmail.com>
2021-04-08 00:23:12 +0300

    Create basic setup file for project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Develop an efficient algorithm for classifying drugs based on their biological activity',
+    author='Oleksandr Sirenko',
+    license='MIT',
+)
